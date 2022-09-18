d671261be68fd1c853d8e956729b3e41b17e7fd1
Luke Bennett <hello@lukebennett.com.au>
2022-09-18 18:44:42 +1000

    Update ESLint config

---
+++ .changeset/chilled-mirrors-train.md
@@ -0,0 +1,5 @@
+---
+'@luke-ui-internal/dev': patch
+---
+
+Add ESLint ignore comment for Common JS file and remove @ts-ignore comment
--- internal/eslint-config-luke-ui/index.js
+++ internal/eslint-config-luke-ui/index.js
@@ -2,52 +2,21 @@ const OFF = 0;
 const WARN = 1;
 const ERROR = 2;
 
-const reactComponentTypeMessage = {
-	message:
-		'This type includes the children prop which is generally wrong, ' +
-		'instead of using this type, type the props of the component',
-};
-
 /** @type {import('@types/eslint').Linter.BaseConfig} */
 module.exports = {
 	parser: '@typescript-eslint/parser',
-	env: {
-		browser: true,
-		es6: true,
-		node: true,
-	},
-	extends: ['next', 'prettier'],
-	plugins: ['@typescript-eslint', 'import', 'simple-import-sort'],
+	plugins: ['@typescript-eslint', 'simple-import-sort'],
+	extends: [
+		'next/core-web-vitals',
+		'prettier',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:import/recommended',
+		'plugin:unicorn/recommended',
+	],
 	settings: {
-		next: { rootDir: ['docs/', 'internal/*/', 'packages/*/'] },
 		react: { version: 'detect' },
 	},
 	rules: {
-		'@next/next/no-html-link-for-pages': OFF,
-		'@typescript-eslint/ban-types': [
-			ERROR,
-			{
-				extendDefaults: false,
-				types: {
-					'Function':
-						'`Function` types are unsafe. Use more specific function types ' +
-						'instead. e.g. (arg: number) => string',
-					'String': {
-						message:
-							'The `String` type refers to the String object which is probably ' +
-							'not what you want, you probably want `string` instead which ' +
-							'refers to the string primitive type.',
-						fixWith: 'string',
-					},
-					'ComponentType': reactComponentTypeMessage,
-					'FC': reactComponentTypeMessage,
-					'SFC': reactComponentTypeMessage,
-					'React.ComponentType': reactComponentTypeMessage,
-					'React.FC': reactComponentTypeMessage,
-					'React.SFC': reactComponentTypeMessage,
-				},
-			},
-		],
 		'@typescript-eslint/consistent-type-imports': WARN,
 		'@typescript-eslint/no-unused-vars': [
 			ERROR,
@@ -63,14 +32,16 @@ module.exports = {
 		'no-restricted-syntax': [
 			ERROR,
 			{
-				// Curious why we have this rule?
-				// - Enums only work for a subset of use cases that unions of string
-				// literals + objects work for and learning one language feature is
-				// easier than learning two language features
-				// - Enums are a new language feature which have runtime semantics which
-				// means they change TypeScript from JS + types to JS + types + extra
-				// language features which is harder to teach without clear advantages
-				// for this specific feature
+				/**
+				 * Curious why we have this rule?
+				 * - Enums only work for a subset of use cases that unions of string
+				 *   literals + objects work for and learning one language feature is
+				 *   easier than learning two language features
+				 * - Enums are a new language feature which have runtime semantics which
+				 *   means they change TypeScript from JS + types to JS + types + extra
+				 *   language features which is harder to teach without clear advantages
+				 *   for this specific feature
+				 */
 				selector: 'TSEnumDeclaration',
 				message: 'Use a union of string literals instead of an enum',
 			},
@@ -102,14 +73,18 @@ module.exports = {
 		'semi': ERROR,
 		'simple-import-sort/exports': ERROR,
 		'simple-import-sort/imports': ERROR,
+		'unicorn/prefer-module': OFF,
+		'unicorn/prevent-abbreviations': OFF,
 		'strict': OFF,
 	},
 	overrides: [
 		{
 			files: ['*.ts', '*.tsx'],
-			extends: ['plugin:@typescript-eslint/recommended'],
 			rules: {
-				// TypeScript already checks for the following things and they cause conflicts
+				/**
+				 * TypeScript already checks for the following things and they cause
+				 * conflicts.
+				 */
 				'import/no-unresolved': OFF,
 				'no-undef': OFF,
 			},
@@ -124,10 +99,12 @@ module.exports = {
 				'**/tests/**/*',
 			],
 			rules: {
-				// Allow importing from packages that aren't listed in package.json for
-				// test and Storybook files
-				// TypeScript will still catch uninstalled imports for us as there will
-				// be no modules or type definitions for them
+				/**
+				 * Allow importing from packages that aren't listed in package.json for
+				 * test and Storybook files.
+				 * TypeScript will still catch uninstalled imports for us as there will
+				 * be no modules or type definitions for them.
+				 */
 				'import/no-extraneous-dependencies': OFF,
 			},
 		},
