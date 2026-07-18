1356709710150f15251f4ebfee37548472441a39
Luke Bennett <hello@lukebennett.com.au>
2026-07-18 15:48:34 +1000

    Simplify cursor example

--- apps/docs/src/examples/overview/cursor-states.tsx
+++ apps/docs/src/examples/overview/cursor-states.tsx
@@ -1,21 +1,16 @@
 import { Box } from '@luke-ui/react/box';
 import { Button } from '@luke-ui/react/button';
-import { Link } from '@luke-ui/react/link';
 import { TextField } from '@luke-ui/react/text-field';
 
 export default function CursorStatesExample() {
 	return (
-		<Box display="grid" gap="400">
-			<Box alignItems="center" display="flex" flexWrap="wrap" gap="300">
-				<Button tone="accent">Button</Button>
-				<Button appearance="ghost">Ghost button</Button>
-				<Link href="#cursor-states" isStandalone>
-					Link
-				</Link>
-				<Button isDisabled>Disabled</Button>
-				<Button isPending>Saving</Button>
+		<Box alignItems="flex-end" display="flex" flexWrap="wrap" gap="300">
+			<Button tone="accent">Save</Button>
+			<Button isDisabled>Save</Button>
+			<Button isPending>Saving</Button>
+			<Box inlineSize="12rem">
+				<TextField label="Name" name="name" />
 			</Box>
-			<TextField label="Project name" name="projectName" placeholder="Luke UI" />
 		</Box>
 	);
 }
