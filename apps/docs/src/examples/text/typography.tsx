70bc9391344334fd26cff0393cf4bb28d73fcadb
Luke Bennett <hello@lukebennett.com.au>
2026-07-26 14:12:39 +1000

    Remove overflow wrapper in typography example

--- apps/docs/src/examples/text/typography.tsx
+++ apps/docs/src/examples/text/typography.tsx
@@ -6,33 +6,31 @@ const sizes = ['100', '200', '300', '400', '500', '600', '700', '800', '900'] as
 
 export default function Typography() {
 	return (
-		<Box overflowX="auto">
-			<Box minInlineSize="45rem">
-				<Box
-					alignItems="flex-end"
-					display="grid"
-					style={{
-						borderBlockEnd: `1px dashed ${vars.color.border.decorative}`,
-						gridTemplateColumns: 'repeat(9, minmax(5rem, 1fr))',
-					}}
-				>
-					{sizes.map((size) => (
-						<Text elementType="div" key={size} size={size} textAlign="center">
-							Aa
-						</Text>
-					))}
-				</Box>
-				<Box
-					display="grid"
-					paddingBlockStart="200"
-					style={{ gridTemplateColumns: 'repeat(9, minmax(5rem, 1fr))' }}
-				>
-					{sizes.map((size) => (
-						<Text color="secondary" elementType="div" key={size} size="100" textAlign="center">
-							{size}
-						</Text>
-					))}
-				</Box>
+		<Box minInlineSize="45rem">
+			<Box
+				alignItems="flex-end"
+				display="grid"
+				style={{
+					borderBlockEnd: `1px dashed ${vars.color.border.decorative}`,
+					gridTemplateColumns: 'repeat(9, minmax(5rem, 1fr))',
+				}}
+			>
+				{sizes.map((size) => (
+					<Text elementType="div" key={size} size={size} textAlign="center">
+						Aa
+					</Text>
+				))}
+			</Box>
+			<Box
+				display="grid"
+				paddingBlockStart="200"
+				style={{ gridTemplateColumns: 'repeat(9, minmax(5rem, 1fr))' }}
+			>
+				{sizes.map((size) => (
+					<Text color="secondary" elementType="div" key={size} size="100" textAlign="center">
+						{size}
+					</Text>
+				))}
 			</Box>
 		</Box>
 	);
