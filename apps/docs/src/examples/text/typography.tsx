81ff4e70a7d221c1dcacdc641a6355cfd9a67ff2
Luke Bennett <hello@lukebennett.com.au>
2026-08-12 10:23:05 +1000

    Fix typography example

--- apps/docs/src/examples/text/typography.tsx
+++ apps/docs/src/examples/text/typography.tsx
@@ -1,28 +1,34 @@
 import { Box } from '@luke-ui/react/box';
 import { Text } from '@luke-ui/react/text';
 import { typeStyles, vars } from '@luke-ui/react/theme';
+import { Fragment } from 'react';
 
 export default () => {
 	return (
 		<Box display="flex" flexDirection="column" gap="300">
-			{typeStyles.map((typography) => (
-				<Box
-					alignItems="flex-end"
-					display="grid"
-					gap="300"
-					key={typography}
-					style={{ gridTemplateColumns: 'max-content minmax(0, 1fr)' }}
-				>
-					<Text color="secondary" elementType="div" typography="caption">
-						{typography}
-					</Text>
-					<Box style={{ borderBlockEnd: `1px dashed ${vars.color.border.decorative}` }}>
-						<Text elementType="div" typography={typography}>
-							Aa
+			<Box
+				alignItems="flex-end"
+				display="grid"
+				elementType="dl"
+				gap="300"
+				style={{ gridTemplateColumns: 'max-content minmax(0, 1fr)' }}
+			>
+				{typeStyles.map((typography) => (
+					<Fragment key={typography}>
+						<Text color="secondary" elementType="dt" typography="caption">
+							{typography}
 						</Text>
-					</Box>
-				</Box>
-			))}
+						<Box
+							elementType="dd"
+							style={{ borderBlockEnd: `1px dashed ${vars.color.border.decorative}` }}
+						>
+							<Text elementType="div" typography={typography}>
+								Aa
+							</Text>
+						</Box>
+					</Fragment>
+				))}
+			</Box>
 		</Box>
 	);
 };
