7881c12294c548a54a1890b2b413dc779222f0e4
Luke Bennett <hello@lukebennett.com.au>
2026-08-12 11:42:29 +1000

    Update emoji example

--- apps/docs/src/examples/emoji/basic.tsx
+++ apps/docs/src/examples/emoji/basic.tsx
@@ -1,16 +1,10 @@
-import { Box } from '@luke-ui/react/box';
 import { Emoji } from '@luke-ui/react/emoji';
 import { Text } from '@luke-ui/react/text';
 
 export default () => {
 	return (
-		<Box display="flex" flexDirection="column" gap="300">
-			<Text>
-				Deployment complete <Emoji emoji="🎉" label="Celebration" />
-			</Text>
-			<Text typography="heading3">
-				Hello <Emoji emoji="👋" label="Waving hand" />
-			</Text>
-		</Box>
+		<Text>
+			Status: <Emoji emoji="✅" label="Complete" />
+		</Text>
 	);
 };
